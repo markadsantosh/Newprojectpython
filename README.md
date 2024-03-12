91e04cbb3b4b43bca9064692f8a11fadafabe613
santosh <santoshmarkad5555@gmail.com>
2024-03-13 00:24:27 +0530

    more files updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# Newprojectpython
-this is a practice files where we can create or updates the files
--- sandy.py
+++
@@ -1,1 +0,0 @@
-this is something for python regarding....like that to do python task...whatever it is write or wrong ..but have to do more tasks to improve ourself
